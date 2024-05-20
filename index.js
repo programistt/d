fafa159e7c2a3dc4a689544bdab3d94916417617
Islom Toirjonov <toirjonovislomjon3@gmail.com>
2024-05-20 12:02:27 +0500

    done

---
+++ index.js
@@ -0,0 +1,11 @@
+let ism = prompt("Ismingizni kiriting : ") 
+let yosh = prompt("Yoshingizni ni kiriting : ")
+let shahar = prompt("Yashaydigon shaharni kiriting : ")
+
+let you = {
+    name: ism,
+    age: yosh,
+    city: shahar,
+};
+
+console.log(you)
